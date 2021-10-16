a43d5fc4d6c29934ce242031f35a16b24ab7bf6c
Kelsey Erickson <kerickson@gmx.com>
2021-10-16 15:42:40 +0000

    Change reserve button to stay active

--- LightBnB_WebApp-master/public/javascript/index.js
+++ LightBnB_WebApp-master/public/javascript/index.js
@@ -3,10 +3,11 @@ $(() => {
     propertyListings.addProperties(json.properties);
     views_manager.show('listings');
 
-    $('.reserve-button').on('click', function() {
+    // To allow the reserve button to stay active using document and the .reserve-button as the selector parameter
+    $(document).on('click','.reserve-button', function() {
       const idData = $(this).attr('id').substring(17);
       views_manager.show('newReservation', idData);
-    })
+    });
 
     $('.review_details').on('click', function() {
       const idData = $(this).attr('id').substring(15);
