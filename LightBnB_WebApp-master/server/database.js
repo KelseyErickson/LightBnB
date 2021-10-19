cd649ba9ec290a831eea9f8f630a3c9c9a310c05
Kelsey Erickson <kerickson@gmx.com>
2021-10-19 17:00:09 +0000

    Add missing semi-colons

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -140,10 +140,10 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `${clause} cost_per_night <= $${queryParams.length} AND cost_per_night >=$${queryParams.length - 1}`;
   } else if (options.minimum_price_per_night) {
     queryParams.push(options.minimum_price_per_night*100);
-    queryString += `${clause} cost_per_night >= $${queryParams.length}`
+    queryString += `${clause} cost_per_night >= $${queryParams.length}`;
   } else if (options.maximum_price_per_night) {
     queryParams.push(options.maximum_price_per_night*100);
-    queryString += `${clause} cost_per_night <= $${queryParams.length}`
+    queryString += `${clause} cost_per_night <= $${queryParams.length}`;
   }
 
   if (options.minimum_rating) {
